9cfee23326329233a51b5ea0eb66980dd22e2d20
Arturo Andres <arturo.anza87@gmail.com>
2026-01-05 20:46:35 +0100

    Tamaño hexagono7

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -94,7 +94,7 @@ export default function Home({ onNavigate }: HomeProps) {
       )}
       
       <div className="app-version">
-        Heptagramas v0.33
+        Heptagramas v0.34
       </div>
     </div>
   );
